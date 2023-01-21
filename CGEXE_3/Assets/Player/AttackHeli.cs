dc197d65b7e9f9aac1de0cc287482951bafdb717
Minamin <minamin37345@gmail.com>
2023-01-22 03:00:12 +0900

    update: update

--- CGEXE_3/Assets/Player/AttackHeli.cs
+++ CGEXE_3/Assets/Player/AttackHeli.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class AttackHeli : Vehicle
 {
+    public GameObject Rotor = null;
+    public float BaseRotateSpeed = 1000.0f;
+    public float MaxRotateSpeed = 5000.0f;
+
+    protected override void VehicleUpdate()
+    {
+        base.VehicleUpdate();
+        if (this.Rotor != null)
+        {
+            var rot = this.Rotor.transform.eulerAngles;
+            rot.y += this.RotateSpeed * Time.deltaTime;
+            this.Rotor.transform.eulerAngles = rot;
+        }
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
--- CGEXE_3/Assets/Player/Pawn.cs
+++ CGEXE_3/Assets/Player/Pawn.cs
@@ -8,13 +8,30 @@ using UnityEngine;
 /// </summary>
 public class Pawn : MonoBehaviour
 {
-    [SerializeField]
-    private bool isactive_ = false;
+    public bool isactive_ = false;
+    public Camera maincamera = null;
+    public List<Camera> cameras = new List<Camera>();
 
     public bool IsActive
     {
         get { return this.isactive_; }
         set { this.isactive_ = value; }
+    }
+
+    public virtual void Possess()
+    {
+        this.isactive_ = true;
+        this.maincamera.gameObject.SetActive(true);
+    }
+
+    public virtual void UnPossess()
+    {
+        this.isactive_ = false;
+        this.maincamera.gameObject.SetActive(false);
+        foreach (var c in this.cameras)
+        {
+            c.gameObject.SetActive(false);
+        }
     }
     
     /// <summary>
